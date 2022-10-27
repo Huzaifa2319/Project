21469b9da42dcf0b236bd1652bbacbf37449af68
fahadBajwaa <fahadbajwa1149@gmail.com>
2022-10-27 15:49:59 +0500

    2nd

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,20 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 const app = express();
 
-const port = 3000;
+app.use(bodyParser.json({limit: "50mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended:true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/a", (req, res) => {
-  res.send("Hello Universe");
-});
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://fahadbajwa:jutt7868@cluster0.b3i8o9l.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser:true, useUnifiedTopology:true
+})
