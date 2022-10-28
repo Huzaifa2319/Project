5ddbb9c54a7fdcd6b334463d224d2b6f0e45eb88
fahadBajwaa <fahadbajwa1149@gmail.com>
2022-10-28 12:31:50 +0500

    3rd MongoDB setup

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,8 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({limit: "50mb", extended:true}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended:true}));
+app.use(bodyParser.json({limit: "20mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 
 app.use(cors());
 
@@ -17,4 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true, useUnifiedTopology:true
-})
+}).then(() => app.listen(PORT, () =>
+console.log('Connection is established and running on port:  ${PORT}')
+)).catch((err) => console.log(err.message));
+
+
+mongoose.set('useFindAndModify',false);
